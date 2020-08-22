29ea2eea2d99f1753880a1d8531759f3454f9902
BOUAZAR MEHDI <mbouazar@rc2k.fr>
2020-08-22 12:58:07 +0100

    add nebular style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,11 +18,13 @@ import { BoardComponent } from './board/board.component';
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'cosmic' }),
     NbLayoutModule,
     NbEvaIconsModule,
-    AppRoutingModule
+    NbButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
